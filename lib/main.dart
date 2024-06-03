20848fa710424af7feabb3f301b55a109e4596ed
mochi-yu <mochi.2225.it@gmail.com>
2024-06-03 19:50:20 +0900

    feat: 近くの教室を表示する処理を追加

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,18 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 
 void main() {
   runApp(const MainApp());
+  setup();
+}
+
+void setup() async {
+  // Bluetoothが有効でパーミッションが許可されるまで待機
+  await FlutterBluePlus.adapterState
+      .where((val) => val == BluetoothAdapterState.on)
+      .first;
 }
 
 Map<String, ScanResult> mapScanResults = {};
@@ -15,9 +25,69 @@ class MainApp extends StatefulWidget {
   State<MainApp> createState() => _MainAppState();
 }
 
+String getRoomName(String str) {
+  if(str.split('-').length > 1) {
+    String roomName = "${str.split('-')[1]}教室";
+    return roomName;
+  }
+
+  return "";
+}
+
+class RoomInfo {
+  late String roomName;
+  late int rssi;
+
+  RoomInfo(this.roomName, this.rssi);
+}
+
 class _MainAppState extends State<MainApp> {
+  RegExp re = RegExp(r'^SUSMAP-[0-9]{3}$');
+
+  String _nearRoom = "確認中...";
+
+  @override
+  void initState() {
+    super.initState();
+    startScan();
+  }
+
+  void startScan() {
+    FlutterBluePlus.startScan(timeout: const Duration(seconds: 1));
+
+    int maxRssi = -1000000;
+    String maxRssiSpot = "";
+
+    FlutterBluePlus.scanResults.listen((results) {
+      RegExp re = RegExp(r'^SUSMAP-[0-9]{3}$');
+      for (ScanResult r in results) {
+        if (re.hasMatch(r.device.platformName)) {
+          if(r.rssi > maxRssi) {
+            maxRssi = r.rssi;
+            maxRssiSpot = r.device.platformName;
+          }
+        }
+      }
+    });
+
+    // Restart the scan every 4 seconds to keep it continuous
+    Future.delayed(const Duration(seconds: 1), () {
+      String roomName = getRoomName(maxRssiSpot);
+
+      if(_nearRoom != roomName && roomName != "") {
+        setState(() {
+          _nearRoom = roomName;
+        });
+      }
+
+      FlutterBluePlus.stopScan();
+      startScan();
+    }); 
+  }
+
   @override
   Widget build(BuildContext context) {
+    debugPrint("build.");
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -25,39 +95,19 @@ class _MainAppState extends State<MainApp> {
         ),
         body: Center(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              ElevatedButton(
-                onPressed: () async {
-                  RegExp re = new RegExp(r'^SUSMAP-[0-9]{3}$');
-                  // スキャン結果のリスナーをセットアップ
-                  var subscription = FlutterBluePlus.scanResults.listen((results) {
-                    if (results.isNotEmpty) {
-                      for (var r in results) {
-                        if (re.hasMatch(r.device.platformName)) {
-                          debugPrint('======');
-                          debugPrint('強度: ${r.rssi}');
-                          debugPrint('デバイス名: ${r.device.platformName}');
-                          debugPrint('UUID: ${r.device.remoteId}');
-                        }
-                      }
-                    }
-                  },
-                  onError: (e) => debugPrint(e));
-
-                  // Bluetoothが有効でパーミッションが許可されるまで待機
-                  await FlutterBluePlus.adapterState
-                      .where((val) => val == BluetoothAdapterState.on)
-                      .first;
-
-                  // スキャン開始
-                  await FlutterBluePlus.startScan();
-                },
-                child: const Text('Start Scan'),
-              ),
+              Text(_nearRoom, style: const TextStyle(fontSize: 50),)
             ]
           )
         ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    FlutterBluePlus.stopScan();
+    super.dispose();
   }
 }
